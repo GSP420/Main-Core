39be3ca7fcda98a1718b29a52a1d46e51e1876d3
Manuel80 <soul_cutter_1980@hotmail.com>
2013-09-28 22:39:54 -0500

    Update MessageManager.cpp

--- MainCore/MessageManager.cpp
+++ MainCore/MessageManager.cpp
@@ -14,10 +14,17 @@ void MessageManager::Update(void)
 	// loop through messages
 		// send them to the proper entity
 		// remove from list
+	While(GetMessage(&Msg,NULL,0,0)>0)
+		{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+		}
 }
 
 void MessageManager::Add(Message message)
 {
 	// set message->msgNext to msgFirst
+	msgNext = msgFirst;
 	// set msgFirst to message
+	msgFirst = message;
 }
