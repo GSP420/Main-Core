b2f6cd262ecadf94be169d63e11bf100abbfe2c1
Alex Larioza <alexlarioza@gmail.com>
2013-10-02 11:35:54 -0700

    Message params should be pointers

--- MainCore/IDispatcher.h
+++ MainCore/IDispatcher.h
@@ -1,6 +1,11 @@
+#pragma once
+#include "Message.h"
+
+class Message;
+
 class IDispatcher
 {
 public:
-	virtual void Recieve(Message message) = 0;
-	virtual void Send(Message message) = 0;
+	virtual void Recieve(Message *message) = 0;
+	virtual void Send(Message *message) = 0;
 };
