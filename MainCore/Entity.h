18fce248a5c963acebacc2bd4112ece21d293d6b
Alex Larioza <alexlarioza@gmail.com>
2013-10-02 11:34:57 -0700

    Removed unneeded params

--- MainCore/Entity.h
+++ MainCore/Entity.h
@@ -6,6 +6,7 @@
 
 class Entity
 {
+public:
 	//pass animation through
 	//*header* *animation*;	
 	int x;
@@ -24,13 +25,13 @@ class Entity
 	Entity(void);
 	~Entity(void);
 
-	void Added(int eID);
-	void Update(int frames);
-	void Render(D3DXRENDER3 Position);
-	void Removed(int eID);
+	void Added();
+	void Update();
+	void Render();
+	void Removed();
 
 	//Getters
-	const RECT GetRect()const { return renderedItem.GetRect(); }
+	//const RECT GetRect()const { return renderedItem.GetRect(); }
 	const int GetID()const { return eID; }
 
 	//Setters
