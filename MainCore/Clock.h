f2b80c88e0e46a05c2abe48ad3a3981dc0f6e5f8
Alex Larioza <alexlarioza@gmail.com>
2013-10-02 11:36:54 -0700

    fixed clock

--- MainCore/Clock.h
+++ MainCore/Clock.h
@@ -1,20 +1,41 @@
+#pragma once
 #include <stdio.h>
 #include <time.h>
+#include <stack>
+
+const int FPS_FRAME_BUFFER = 10;
 
 class Clock 
 {
-	float  elapsed;
-	int hours;
-	int seconds;
+private:
+	int elapsedTime;
+	int elapsedLast;
+	int frameLast;
+	int frameTime;
+	int renderStart;
+	int updateStart;
+	int frames[FPS_FRAME_BUFFER];
+
+	// stats
+	float elapsed;
+	float fps;
+	float render;
+	float update;
+
+public:
+	Clock(void);
+	~Clock(void);
+
+	void UpdateElapsed(void);
+	void UpdateFPS(void);
+	void StartUpdate(void);
+	void EndUpdate(void);
+	void StartRender(void);
+	void EndRender(void);
+
+	// GETTERS
+	float GetElapsed() { return this->elapsed; }
+	float GetFPS() { return this->fps; }
+	float GetRender() { return this->render; }
+	float GetUpdate() { return this->update; }
 };
-
-int main () {
-	time_t rawtime; 
-	struct tm * timeinfo;
-
-	time ( &rawtime );
-	timeinfo = localtime ( &rawtime );
-	printf ( "Current local time and date: %s", ctime (&rawtime) );
-
-	return 0;
-}
